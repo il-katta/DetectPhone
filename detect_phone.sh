7588a71868d92d594365601c9a67e468102d504d
il-katta <ilkatta88@gmail.com>
2013-03-14 00:15:35 +0100

    Update detect_phone.sh

--- detect_phone.sh
+++ detect_phone.sh
@@ -4,15 +4,32 @@
 # date: 6 Feb 2012
 
 BTHW="xx:xx:xx:xx:xx:xx"  # Enter your Phone Bluetooth hardware address
-LOCKPRG="gnome-screensaver"
-UNLOCK_CMD="gnome-screensaver-command -d"
-SLEEP=5
+DESKTOP="gnome"
+
+
+SLEEP=5 
 
 ME=`whoami`
+
+case "$DESKTOP" in
+	"gnome" | "GNOME")
+		UNLOCK_CMD="gnome-screensaver-command -d"
+		STATE_CMD="gnome-screensaver-command -q | grep \" active$\""
+		export LC_ALL="en_US.utf-8"
+		;;
+	"kde" | "KDE" )
+		LOCKPRG="kscreenlocker"
+		UNLOCK_CMD="kill `pgrep -u $ME $LOCKPRG | head -1`"
+		STATE_CMD="pgrep -u $ME $LOCKPRG"
+		;;
+	*) 
+		exit 1 
+		;;
+esac
+
 while true
 do	# Run only if screen is locked
-	STATE=`LANG=en_US.utf8 gnome-screensaver-command -q 2>/dev/null`
-	if (echo $STATE | grep " active$" &>/dev/null); then
+	if ( $STATE_CMD &>/dev/null ); then
 		# Ping the phone (needs sudo permissions
 		# chmod u+s /usr/bin/l2ping
 		l2ping -t 5 -c 1 $BTHW >/dev/null 2>/dev/null
